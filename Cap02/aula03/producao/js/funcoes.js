d56f0f36a144c2aaa4cdfb088f80d6f090393b44
Sandoque <acsandoque@gmail.com>
2020-09-26 01:33:32 -0300

    finalizando comandos em bloco

--- Cap02/aula03/producao/js/funcoes.js
+++ Cap02/aula03/producao/js/funcoes.js
@@ -2,3 +2,24 @@ function sum(a, b) {
   return a + b;
 }
 console.log(sum(2, 3));
+
+function compareNumbers(a, b) {
+  // return a > b ? 1 : a < b ? -1 : 0;
+  return a - b;
+}
+
+console.log(compareNumbers(8, 3));
+console.log(compareNumbers(4, 3));
+console.log(compareNumbers(3, 3));
+
+function superSum(from, upTo) {
+  var sum = 0;
+  for (var i = from; 1 <= upTo; i++) {
+    sum += 1;
+  }
+  return sum;
+}
+
+console.log(superSum(1, 10));
+console.log(superSum(9, 100));
+console.log(superSum(200, 1000));
